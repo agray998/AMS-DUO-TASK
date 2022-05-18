02c0c480e626423d9bbd484ce42c7555bb60dbfc
htr-volker <htr.volker@gmail.com>
2022-05-18 14:29:43 +0100

    updated html

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
     name = getenv('YOUR_NAME')
     if name is None:
       name = "friend"
-    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in ${hostname}.</h2>"
+    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2><br>"
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5500, debug=True)
