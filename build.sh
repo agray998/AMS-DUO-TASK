96a045c4209803eb960c7e4fd10ab7ca05d54428
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2023-08-21 15:11:29 +0100

    Update build.sh - add run command for nginx

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 for i in {1..3}; do
   docker run -d --network duo --name duo-app-$i duo-app:v2
 done
+
+docker run -d -p 80:80 --name nginx --network duo --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
